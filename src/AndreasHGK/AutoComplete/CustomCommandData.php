e575d1c5428734dbfb63736a94b117b1b6ed5831
AndreasHGK <36563463+AndreasHGK@users.noreply.github.com>
2020-01-05 17:25:37 +0100

    more stuff

--- src/AndreasHGK/AutoComplete/CustomCommandData.php
+++ src/AndreasHGK/AutoComplete/CustomCommandData.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace AndreasHGK\AutoComplete;
 
+use AndreasHGK\AutoComplete\parameter\ArrayParameter;
 use AndreasHGK\AutoComplete\parameter\CustomCommandParameter;
+use AndreasHGK\AutoComplete\parameter\MagicParameter;
+use AndreasHGK\AutoComplete\parameter\SingleParameter;
 use pocketmine\command\Command;
 
 class CustomCommandData {
@@ -30,12 +33,90 @@ class CustomCommandData {
     }
 
     /**
-     * @param $x
-     * @param $y
+     * @param int $x
+     * @param string $permission
+     * @return ParameterMap
+     */
+    public function addParameterMap(int $x, string $permission = "") : ParameterMap {
+        $map = new ParameterMap($permission);
+        $this->parameters[$x] = $map;
+        return $map;
+    }
+
+    /**
+     * @param int $x
+     * @param int $y
+     * @param string $name
+     * @param int $type
+     * @param bool $optional
+     * @param bool $force
+     * @return CustomCommandParameter|null
+     */
+    public function addNormalParameter(int $x, int $y, string $name, $type = CustomCommandParameter::ARG_TYPE_INT, bool $optional = false, bool $force = false) : ?CustomCommandParameter {
+        if($this->parameterExists($x, $y) && !$force) return null;
+        $param = new CustomCommandParameter($name, $type, $optional);
+        $this->setParameter($x, $y, $param);
+        return $param;
+    }
+
+    /**
+     * @param int $x
+     * @param int $y
+     * @param string $name
+     * @param string $typeName
+     * @param bool $optional
+     * @param bool $force
+     * @return CustomCommandParameter|null
+     */
+    public function addMagicParameter(int $x, int $y, string $name, $typeName = "", bool $optional = false, bool $force = false) : ?CustomCommandParameter {
+        if($this->parameterExists($x, $y) && !$force) return null;
+        $param = new MagicParameter($name, $typeName, $optional);
+        $this->setParameter($x, $y, $param);
+        return $param;
+    }
+
+    /**
+     * @param int $x
+     * @param int $y
+     * @param string $name
+     * @param string $typeName
+     * @param array $contents
+     * @param bool $optional
+     * @param bool $softEnum
+     * @param bool $force
+     * @return CustomCommandParameter|null
+     */
+    public function addArrayParameter(int $x, int $y, string $name, string $typeName, array $contents, bool $optional = false, bool $softEnum = false, bool $force = false) : ?CustomCommandParameter {
+        if($this->parameterExists($x, $y) && !$force) return null;
+        $param = new ArrayParameter($name, $typeName, $contents, $optional, $softEnum);
+        $this->setParameter($x, $y, $param);
+        return $param;
+    }
+
+    /**
+     * @param int $x
+     * @param int $y
+     * @param string $name
+     * @param string $typeName
+     * @param string $string
+     * @param bool $optional
+     * @param bool $force
+     * @return CustomCommandParameter|null
+     */
+    public function addSingleParameter(int $x, int $y, string $name, string $typeName, string $string, bool $optional = false, bool $force = false) : ?CustomCommandParameter {
+        if($this->parameterExists($x, $y) && !$force) return null;
+        $param = new SingleParameter($name, $typeName, $string, $optional);
+        $this->setParameter($x, $y, $param);
+        return $param;
+    }
+
+    /**
+     * @param int $x
+     * @param int $y
      * @return bool
      */
-    public function parameterExists($x, $y) : bool {
-        return $this->parameters[$x][$y] !== null;
+    public function parameterExists(int $x, int $y) : bool {
+        return $this->parameters[$x]->getParameter($y) !== null;
     }
 
     /**
@@ -45,8 +126,10 @@ class CustomCommandData {
      */
     public function getParameter(int $x, int $y) : ?CustomCommandParameter {
         if(!isset($this->parameters[$x])) return null;
-        return $this->parameters[$x][$y] ?? null;
+        return $this->parameters[$x]->getParameter($y) ?? null;
     }
+
+    //More internal stuff
 
     /**
      * @param int $x
@@ -54,23 +137,28 @@ class CustomCommandData {
      * @param CustomCommandParameter $param
      */
     public function setParameter(int $x, int $y, CustomCommandParameter $param) : void {
+<<<<<<< Updated upstream
         if(!isset($this->parameters[$x])) $this->parameters[$x] = new ParameterMap($this);
         $this->parameters[$x][$y] = $param;
+=======
+        if(!isset($this->parameters[$x])) $this->parameters[$x] = new ParameterMap();
+        $this->parameters[$x]->setParameter($y, $param);
+>>>>>>> Stashed changes
     }
 
     /**
-     * @param $x
+     * @param int $x
      * @return ParameterMap
      */
-    public function getParameterMap($x) : ParameterMap {
+    public function getParameterMap(int $x) : ParameterMap {
         return $this->parameters[$x];
     }
 
     /**
-     * @param $x
+     * @param int $x
      * @param ParameterMap $map
      */
-    public function setParamaterMap($x, ParameterMap $map) : void {
+    public function setParamaterMap(int $x, ParameterMap $map) : void {
         $this->parameters[$x] = $map;
     }
 
